1d46c4dc1094cf4ffc8f98e31e931a6fd5f4eca5
MasterPrab <16895.mnr@gmail.com>
2024-10-30 14:55:26 +0700

    LogowithLink

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,19 +1,27 @@
-import styles from './topmenu.module.css'
+import styles from './topmenu.module.css';
 import Image from 'next/image';
 import TopMenuItem from './TopMenuItem';
+import Link from 'next/link';
 
-export default function TopMenu(){
-    return(
-        <div className= {styles.menucontainer} >
-            <Image src = {'/img/logo.jpg'} className = {styles.logoimg} 
-            alt = 'logo' 
-            height={0} 
-            width = {0} 
-            sizes = '100vh'/>
-            <TopMenuItem title='Select Course' pageRef='/course'/>
-            <TopMenuItem title='Your Reservation' pageRef='/reservation'/>
-            <TopMenuItem title='about us' pageRef='/about'/>
+export default function TopMenu() {
+  return (
+    <div className={styles.menucontainer}>
+      {/* Logo as a clickable link to the home page */}
+      <Link href="/">
+        <Image 
+          src="/img/messagelogo.jpg" 
+          className={styles.logoimg} 
+          alt="logo" 
+          width={50}  // Set appropriate dimensions
+          height={50} 
+          priority // Ensures faster load time
+        />
+      </Link>
 
-        </div>
-    );
+      {/* Navigation Menu Items */}
+      <TopMenuItem title="Select Course" pageRef="/course" />
+      <TopMenuItem title="Your Reservation" pageRef="/reservation" />
+      <TopMenuItem title="about us" pageRef="/about" />
+    </div>
+  );
 }
